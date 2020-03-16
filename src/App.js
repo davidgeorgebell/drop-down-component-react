c52367abfa8e711a027e4139081af0a3b6bb7595
davidgeorgebell <bell.dave93@gmail.com>
2020-03-16 10:12:31 +0800

    Dropdown compoennt

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from 'react';
 
+import Dropdown from './Dropdown';
+
+const data = [
+  {
+    name: 'Wave Racer',
+    id: 1
+  },
+  {
+    name: 'Mario Kart',
+    id: 2
+  },
+  {
+    name: 'Jak and Daxter',
+    id: 3
+  }
+];
+
 function App() {
   return (
     <div className='App'>
-      <main></main>
+      <main>
+        <Dropdown
+          title='Pick some games'
+          items={data}
+          /* For multiSelect === multiSelect */
+        />
+      </main>
     </div>
   );
 }
